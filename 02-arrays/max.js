f869bc7a69f461b0e1733d8a3595b76ef0edbd13
andrew-ksv <127143251+andrew-ksv@users.noreply.github.com>
2026-09-14 13:08:43 +0300

    Create max.js

---
+++ 02-arrays/max.js
@@ -0,0 +1,15 @@
+// Find the largest number in an array
+function getMaxFromArray(array) {
+  let max = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(getMaxFromArray([3, 7, 2, 9, 4])); // 9
+console.log(getMaxFromArray([-5, -2, -10])); // -2
