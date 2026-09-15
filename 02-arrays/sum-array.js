c4bf0c800f67999fb4a6904ff02031c73c02df85
andrew-ksv <127143251+andrew-ksv@users.noreply.github.com>
2026-09-15 12:43:32 +0300

    Create sum-array.js

---
+++ 02-arrays/sum-array.js
@@ -0,0 +1,12 @@
+function sumArray(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
+console.log(sumArray([10, 20, 30])); // 60
