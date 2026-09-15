a0fa236090783a2bc7c895bbe9a0ee8e2b98c725
andrew-ksv <127143251+andrew-ksv@users.noreply.github.com>
2026-09-15 12:46:40 +0300

    Create average-array.js

---
+++ 02-arrays/average-array.js
@@ -0,0 +1,12 @@
+function averageArray(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum / array.length;
+}
+
+console.log(averageArray([2, 4, 6])); // 4
+console.log(averageArray([10, 20, 30, 40])); // 25
